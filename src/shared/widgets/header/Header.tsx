59f1504958484e8bc7e71af70ac5f4678e2a3f60
Aniket singh <aniket.singh9322@gmail.com>
2024-06-17 21:36:07 +0530

    basicsSetup

---
+++ src/shared/widgets/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full sticky top-0 z-[999] border-b border-b[#000] px-10 flex itema-center justify-between h-[80px] bg-white text-black'></div>
+  )
+}
+
+export default Header
