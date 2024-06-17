63edb5569f32cb1a0bb68161cf6e25c1bb1a6ee4
Aniket singh <aniket.singh9322@gmail.com>
2024-06-17 22:19:53 +0530

    HeaderBasics

--- src/shared/widgets/header/Header.tsx
+++ src/shared/widgets/header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full sticky top-0 z-[999] border-b border-b[#000] px-10 flex itema-center justify-between h-[80px] bg-white text-black'></div>
+    <div className='w-full sticky top-0 z-[999] border-b border-b[#000] px-10 flex itema-center justify-between h-[80px] bg-white text-black'>TEST</div>
   )
 }
 
